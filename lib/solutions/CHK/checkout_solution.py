aeaf42d25fad6118b3ac2838af002d2857bd0ba7
sbx_user1051 <sbx_user1051@169.254.45.161>
2024-07-10 21:57:46 +0000

    Wed Jul 10 21:57:46 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,11 +1,31 @@
 
-
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+sku_prices = {
+    'A': 50,
+    'B': 30,
+    'C': 20,
+    'D': 15
+}
+
+promotions = {
+    '3A': 20,
+    '2B': 15
+}
+
+bill = 0
+
+
 def checkout(skus: str) -> int:
-    products = list(str) # converting to the list to easier operate on it
-    basket = {str, int}
+    products = list(str)  # converting to the list to easier operate on it
+    basket = {}
     for product in products:
         if product in basket.keys():
             basket[product] += 1
-    raise NotImplementedError()
+        else:
+            basket[product] = 1
+
+def count_basket(basket: dict) -> int:
+    for item in basket:
+        bill += sku_prices[item]*item
