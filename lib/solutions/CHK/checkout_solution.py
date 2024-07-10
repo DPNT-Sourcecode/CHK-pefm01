42b0667ff45ee5c32e4888762651f5e4ec6daa66
sbx_user1051 <sbx_user1051@169.254.45.161>
2024-07-10 21:45:46 +0000

    Wed Jul 10 21:45:46 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,6 +3,10 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus: str) -> int:
-    
+    products = list(str) # converting to the list to easier operate on it
+    basket = {}
+    for product in products:
+        if 
     raise NotImplementedError()
+
 
