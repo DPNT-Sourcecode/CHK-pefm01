8e18eb69f482bc64729a2e981d02d0a67e641f0d
sbx_user1051 <sbx_user1051@169.254.45.161>
2024-07-10 21:48:46 +0000

    Wed Jul 10 21:48:46 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,9 +4,11 @@
 # skus = unicode string
 def checkout(skus: str) -> int:
     products = list(str) # converting to the list to easier operate on it
-    basket = {}
+    basket = {str, int}
     for product in products:
-        if 
+        if product in basket.keys():
+            basket[product] += 1
     raise NotImplementedError()
+
 
 
