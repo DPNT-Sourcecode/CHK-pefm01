0560cba0964b113acc69b3c6b15b273551afeb78
sbx_user1051 <sbx_user1051@169.254.45.161>
2024-07-10 22:30:46 +0000

    Wed Jul 10 22:30:46 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -41,7 +41,8 @@ def count_basket(basket: dict) -> int:
     for item in basket:
         try:
             bill += sku_prices[item] * basket[item]  # increasing bill for product_price * ammount in baskter
-        
+        except:
+            print(f"product that you added to the list: {item} is not for sale. Please put it away.")
     return apply_promotions(basket, bill)
 
 
@@ -51,8 +52,9 @@ def apply_promotions(basket: dict, bill: int) -> int:
         for promo in actual_promotions:
             if promo['sku'] == item:  # if current object is in promotion we need to apply the discount
                 # if int is more than 0, it will apply promo to the basket
-                bill -= int(basket[item]/promo['amount']) * promo['discount']
+                bill -= int(basket[item] / promo['amount']) * promo['discount']
                 break
     return bill
+
 
 
