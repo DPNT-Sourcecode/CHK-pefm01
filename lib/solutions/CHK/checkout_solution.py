41a50ed00e0d706462925c50fefebc92dd129685
sbx_user1051 <sbx_user1051@169.254.52.253>
2024-07-10 23:25:11 +0000

    Wed Jul 10 23:25:11 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -99,9 +99,9 @@ def apply_free_products(free_promo: json, item: object, bill: int, basket: dict)
 
 
 if __name__ == '__main__':
-    # print(checkout("AAAAA")) # 200
-    # print(checkout("AAAAAA")) # 250
-    # print(checkout("AAAAAAA")) # 300
+    print(checkout("AAAAA")) # 200
+    print(checkout("AAAAAA")) # 250
+    print(checkout("AAAAAAA")) # 300
     print(checkout("AAA"))  # 130
     print(checkout("AAAA"))  # 180
-    # print(checkout("AAAAAAAA"))  # 330
+    print(checkout("AAAAAAAA"))  # 330
