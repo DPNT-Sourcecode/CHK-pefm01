5a599cacd6f73f50b7814387cb34a643bdb8967a
sbx_user1051 <sbx_user1051@169.254.45.161>
2024-07-10 21:31:46 +0000

    Wed Jul 10 21:31:46 UTC 2024

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,11 +1,11 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
-def sum(x: int, y: int) -> int:
+def compute(x: int, y: int) -> int:
     if 0 <= x <= 100 and 0 <= y <= 100:
         print("Both values are correct")
+        return x+y
     else:
         raise ValueError('Both values needs to be in range 0-100')
-except ValueError as e:
-    print(e)
+
 
 
 
