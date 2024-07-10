fa54d23d067a2de239860935d38b560a426210ff
sbx_user1051 <sbx_user1051@169.254.45.161>
2024-07-10 21:25:46 +0000

    Wed Jul 10 21:25:46 UTC 2024

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,8 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
-def compute(x, y):
+def sum(x: int, y: int) -> int:
+    if 0 <= x <= 100 and 0 <= y <= 100:
+        print("Both values are correct")
+    else
+        
     raise NotImplementedError()
+
