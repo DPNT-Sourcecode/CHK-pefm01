7e83c5b3b66bdb7076670499909c16dd38e5294f
sbx_user1051 <sbx_user1051@169.254.45.161>
2024-07-10 21:28:46 +0000

    Wed Jul 10 21:28:46 UTC 2024

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -2,7 +2,10 @@
 def sum(x: int, y: int) -> int:
     if 0 <= x <= 100 and 0 <= y <= 100:
         print("Both values are correct")
-    else
-        
-    raise NotImplementedError()
+    else:
+        raise ValueError('Both values needs to be in range 0-100')
+except ValueError as e:
+    print(e)
+
+
 
