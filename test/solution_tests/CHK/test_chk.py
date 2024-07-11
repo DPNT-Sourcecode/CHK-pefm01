53f4f400700384f9f5f8690a677f9c5783a87157
sbx_user1051 <sbx_user1051@169.254.57.37>
2024-07-11 08:53:05 +0000

    Thu Jul 11 08:53:05 UTC 2024

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -1,10 +1,27 @@
 
-from lib.solutions.CHK.checkout_solution import count_basket, apply_promotions, apply_discounts
+from lib.solutions.CHK.checkout_solution import count_basket, apply_promotions, checkout
 
 
 class TestChk():
     def test_chk(self):
         assert count_basket({'A': 4, 'B': 2, 'E': 3}) == 315
 
-    def test_chk_apply_promotion(selfs):
-        assert apply_promotions({'A': 4, 'B': 2, 'E': 3}) == 315
+
+    def check_full_solution(self):
+        assert checkout("AAAAA") == 200
+        assert checkout("AAAAAA") == 250
+        assert checkout("AAAAAAA") == 300
+        assert checkout("AAA") == 130
+        assert checkout("AAAA") == 180
+        assert checkout("AAAAAAAA") == 330
+        assert checkout("AAAABBEEE") == 330
+        assert checkout("AAAAAAAAAA") == 400
+        assert checkout("EEEEBB") == 160
+        assert checkout("BEBEEE") == 160
+        assert checkout("FF") == 20
+        assert checkout("FFF") == 20
+        assert checkout("FFFF") == 30
+
+    # def test_chk_apply_promotion(selfs):
+    #     assert apply_promotions({'A': 4, 'B': 2, 'E': 3}) == 315
+
