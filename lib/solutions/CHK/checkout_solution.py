cacd2166073a57c3a300408f0013855a6c8e38c0
sbx_user1051 <sbx_user1051@169.254.101.101>
2024-07-11 18:37:36 +0000

    Thu Jul 11 18:37:36 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -195,6 +195,7 @@ def check_group_discounts(skus: str):
     group_discounts_load = json.loads(group_discounts)
     bill = 0
     ordered_pricing = sorted(sku_prices.items(), key=lambda x: x[1], reverse=True)
+    
     for group in group_discounts_load:
         count = 0
         for product in skus:
@@ -225,4 +226,5 @@ if __name__ == '__main__':
     # print(checkout("FFF"))  # 20
     # print(checkout("FFFF"))  # 30
     print(checkout("XYZZZXY"))
+
 
