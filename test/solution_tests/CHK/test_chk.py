31cc2851a96e3880c8faf3f1db20b0eae0d789be
sbx_user1051 <sbx_user1051@169.254.57.37>
2024-07-11 09:01:13 +0000

    Thu Jul 11 09:01:13 UTC 2024

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -6,24 +6,48 @@ class TestChk():
     def test_chk(self):
         assert count_basket({'A': 4, 'B': 2, 'E': 3}) == 330
 
-
-    def check_full_solution(self):
+    def check_full_solution1(self):
         assert checkout("AAAAA") == 200
-        # assert checkout("AAAAAA") == 250
-        # assert checkout("AAAAAAA") == 300
-        # assert checkout("AAA") == 130
-        # assert checkout("AAAA") == 180
-        # assert checkout("AAAAAAAA") == 330
-        # assert checkout("AAAABBEEE") == 330
-        # assert checkout("AAAAAAAAAA") == 400
-        # assert checkout("EEEEBB") == 160
-        # assert checkout("BEBEEE") == 160
-        # assert checkout("FF") == 20
-        # assert checkout("FFF") == 20
-        # assert checkout("FFFF") == 30
+
+    def check_full_solution2(self):
+        assert checkout("AAAAAA") == 250
+
+    def check_full_solution3(self):
+        assert checkout("AAAAAAA") == 300
+
+    def check_full_solution4(self):
+        assert checkout("AAA") == 130
+
+    def check_full_solution5(self):
+        assert checkout("AAAA") == 180
+
+    def check_full_solution6(self):
+        assert checkout("AAAAAAAA") == 330
+
+    def check_full_solution7(self):
+        assert checkout("AAAABBEEE") == 330
+
+    def check_full_solution8(self):
+        assert checkout("AAAAAAAAAA") == 400
+
+    def check_full_solution9(self):
+        assert checkout("EEEEBB") == 160
+
+    def check_full_solution10(self):
+        assert checkout("BEBEEE") == 160
+
+    def check_full_solution11(self):
+        assert checkout("FF") == 20
+
+    def check_full_solution12(self):
+        assert checkout("FFF") == 20
+
+    def check_full_solution13(self):
+        assert checkout("FFFF") == 30
 
     def test_chk_apply_promotion(selfs):
         assert apply_promotions({'A': 4, 'B': 2, 'E': 3}, 365) == 315
+
 
 
 
