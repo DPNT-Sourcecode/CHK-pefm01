7cac7f5d58e8800d12089cb865187bfe21cbb8d9
sbx_user1051 <sbx_user1051@169.254.57.37>
2024-07-11 08:19:38 +0000

    Thu Jul 11 08:19:38 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -65,10 +65,10 @@ def count_basket(basket: dict) -> int:
 def apply_promotions(basket: dict, bill: int) -> int:
     actual_promotions = json.loads(promotions)  # loading current promotions
     free_promo = json.loads(free_products)  # loading free products
-    basket_for_discounts = basket.copy()
+    basket_for_discounts = basket
     for item in basket:
-        bill, basket_for_discounts = apply_discounts(actual_promotions, item, bill, basket_for_discounts)
         bill = apply_free_products(free_promo, item, bill, basket)
+        bill, basket_for_discounts = apply_discounts(actual_promotions, item, bill, basket_for_discounts)
     return bill
 
 
@@ -99,11 +99,15 @@ def apply_free_products(free_promo: json, item: object, bill: int, basket: dict)
 
 
 if __name__ == '__main__':
-    # print(checkout("AAAAA")) # 200
-    # print(checkout("AAAAAA")) # 250
-    # print(checkout("AAAAAAA")) # 300
-    # print(checkout("AAA"))  # 130
-    # print(checkout("AAAA"))  # 180
-    # print(checkout("AAAAAAAA"))  # 330
+    print(checkout("AAAAA")) # 200
+    print(checkout("AAAAAA")) # 250
+    print(checkout("AAAAAAA")) # 300
+    print(checkout("AAA"))  # 130
+    print(checkout("AAAA"))  # 180
+    print(checkout("AAAAAAAA"))  # 330
     print(checkout("AAAABBEEE"))  # 330
+    print(checkout("AAAAAAAAAA"))  # 400
+    print(checkout("EEEEBB")) # 160
+    print(checkout("BEBEEE")) # 160
+
 
